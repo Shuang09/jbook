5a0cb31c8fc594295411d7929d11d6c042167c24
Shuhang09 <yshuhang09@gmail.com>
2023-10-10 15:46:50 -0400

    Adding some css styling to md editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,22 @@
-import { useState, useEffect } from "react";
+import './text-editor.css';
+import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC =() => {
+    const ref = useRef<HTMLDivElement | null>(null);
     const [editing, setEditing] = useState(false);
+    const [value, setValue] = useState('# Header');
 
     useEffect(() => {
-        const listener = () =>{
+        const listener = (event: MouseEvent) => {
+
+            if (
+                ref.current &&
+                event.target &&
+                ref.current.contains(event.target as Node)) {
+                return;
+            }
+
             setEditing(false);
         };
 
@@ -13,21 +24,22 @@ const TextEditor: React.FC =() => {
 
         return () => {
             document.removeEventListener('click', listener, {capture: true});
-        }
-
+        };
     }, []);
 
     if (editing) {
         return (
-            <div>
-                <MDEditor />
+            <div className="text-editor" ref ={ref}>
+                <MDEditor value={value} onChange = {(v) => setValue(v || '')} />
             </div>
         );
     }
 
     return (
-    <div onClick={() => setEditing(true)}>
-        <MDEditor.Markdown source={ '# Header'} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+        <div>
+        <MDEditor.Markdown source={ value} />
+        </div>
     </div> 
     );
 };
