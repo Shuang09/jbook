5954ef142f9996f7c6a4cee6ad6cfe4c5519d962
Shuhang09 <yshuhang09@gmail.com>
2023-10-11 21:06:05 -0400

    Introducing 'Immer' for state update

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-// import CodeCell from './components/code-cell';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor/>
-    </div>
+    <Provider store ={store}>
+       <div>
+        <CellList/>
+      </div>
+    </Provider>
+     
   );
 };
 
